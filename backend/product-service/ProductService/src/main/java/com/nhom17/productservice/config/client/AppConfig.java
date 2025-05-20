7671266b23f3704fbef647a2ad14b3b9dad9f6d1
duytien2608 <nguyenduytien2905@gmail.com>
2025-05-20 15:27:07 +0700

    ProductService

---
+++ backend/product-service/ProductService/src/main/java/com/nhom17/productservice/config/client/AppConfig.java
@@ -0,0 +1,13 @@
+package com.nhom17.productservice.config.client;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
---
+++ backend/product-service/ProductService/src/main/java/com/nhom17/productservice/repository/CategoryRepositoryPagingAndSorting.java
@@ -0,0 +1,15 @@
+package com.nhom17.productservice.repository;
+
+
+import com.nhom17.productservice.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CategoryRepositoryPagingAndSorting extends PagingAndSortingRepository<Category, Integer> {
+
+    @Query("SELECT c FROM Category c")
+    Page<Category> findAllPagedAndSortedCategories(Pageable pageable);
+
+}
