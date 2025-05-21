1c9f7b0b79cca87662d66842bae4c3003e51fe3e
NguyenManhHung-code <92658219+NguyenManhHung-code@users.noreply.github.com>
2025-05-22 00:31:09 +0700

    notification-service
    notification-service

---
+++ backend/notification-service/src/main/java/notificationservice/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package notificationservice.repository;
+
+import notificationservice.entity.Notification;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+
+}
---
+++ backend/notification-service/src/main/java/notificationservice/repository/PaymentRepository.java
@@ -0,0 +1,10 @@
+package notificationservice.repository;
+
+import notificationservice.entity.Payment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends MongoRepository<Payment, Integer> {
+
+}
